2b0cc3e6048acafd5209533b043f909fd1df61d9
misbahx <misbahx.id@gmail.com>
2025-07-17 11:26:18 +0700

    feat: add UserList component and fetch users from DummyJSON API

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import Counter from "@/components/Counter";
+import UserList from "@/components/UserList";
 
 function App() {
   return (
     <div>
-      <Counter />
+      <UserList />
     </div>
   );
 }
